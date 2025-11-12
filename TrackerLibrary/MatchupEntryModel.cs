b69e2d17d619105666181823223805e84206af53
HugoABSSMendes <hugo_skm@outlook.com>
2025-11-12 11:30:17 +0000

    Class Library Creation

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+
+        /// <summary>
+        /// Represents one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+
+        /// <summary>
+        /// Represents the score for a particular team.
+        /// </summary>
+        public double Score { get; set; }
+
+
+        /// <summary>
+        /// Represents the matchup that a team came from.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
